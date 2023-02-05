338ae589e325d9bbfde76a5613be2caaa8e548ea
SumiAkter2 <akteralisha36@gmail.com>
2023-02-05 14:59:10 +0600

    add reset input

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import { computeHeadingLevel } from "@testing-library/react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
   const handleSubmit = (e) => {
     e.preventDefault();
     const name = e.target.name.value;
     const vill = e.target.vill.value;
     const user = { name, vill };
-    console.log(user);
+    e.target.reset();
+
     fetch("http://localhost:5000/users", {
       method: "POST",
       headers: {
@@ -16,18 +23,28 @@ function App() {
       body: JSON.stringify(user),
     })
       .then((res) => res.json())
-      .then((data) => console.log(data))
+      .then((data) => {
+        const newUser = [...users, data];
+        setUsers(newUser);
+        console.log(data);
+      })
       .catch((err) => console.log(err));
   };
   return (
     <div className="App">
       <div>
         <h1>Node Mongo Crud Clients Site</h1>
-
+        <h1> {users.length}</h1>
+        {users.map((user) => (
+          <li key={users._id}>
+            {user.name} ****{user.vill}****
+          </li>
+        ))}
         <form onSubmit={handleSubmit}>
           <input type="text" name="name" placeholder="Name" />
           <br />
           <input type="text" name="vill" placeholder="Village" />
+          <br />
           <button>Submit</button>
         </form>
       </div>
